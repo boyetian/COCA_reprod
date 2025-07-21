ee62842de26f50bec49dfa6a3efc921287e9d138
chenqiang <u3637089@connect.hku.hk>
2025-07-21 10:17:44 +0000

    Add .gitignore and update README; modify download_data.sh for dataset extraction

--- data/download_data.sh
+++ data/download_data.sh
@@ -4,12 +4,15 @@
 # Note: You will need to have an account on the ImageNet website to download the data.
 
 # Create a data directory if it doesn't exist
-mkdir -p data
+# mkdir -p data
 
 # Download the ImageNet-C dataset
 # You will need to replace the following URL with the actual download link from the ImageNet website.
 # The file is ~76GB.
-wget -c https://zenodo.org/record/2235448/files/imagenet-c.tar -P data
+wget -c https://zenodo.org/record/2235448/files/imagenet-c.tar 
+
+# Download the Tiny ImageNet-C dataset for smaller experiments
+# wget https://zenodo.org/records/2536630/files/Tiny-ImageNet-C.tar?download=1
 
 # Extract the dataset
-tar -xvf data/imagenet-c.tar -C data 
+tar -xvf imagenet-c.tar
