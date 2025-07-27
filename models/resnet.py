47df7c534056b8f305173f47899e28a60b0093fd
boyetian <boyetian@connect.hku.hk>
2025-07-27 12:52:22 +0800

    Revert "added a jupyter file, made some modifications to make sure main program is runnable"
    This reverts commit 735eb405ca049c9bc2b6242d7fea1baecf458a21.

--- models/resnet.py
+++ models/resnet.py
@@ -2,6 +2,4 @@ import timm
 
 def get_resnet(model_name='resnet50', pretrained=True):
     model = timm.create_model(model_name, pretrained=pretrained)
-    return model
-
-resnet50 = lambda pretrained=True: get_resnet('resnet50', pretrained)
+    return model 
--- models/vit.py
+++ models/vit.py
@@ -6,6 +6,4 @@ def get_vit(model_name='vit_base_patch16_224', pretrained=True):
 
 def get_mobilevit(model_name='mobilevit_s', pretrained=True):
     model = timm.create_model(model_name, pretrained=pretrained)
-    return model
-
-vit_base_patch16_224 = lambda pretrained=True: get_vit('vit_base_patch16_224', pretrained)
+    return model 
