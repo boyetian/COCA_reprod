7ece536906d32ab4670bac4a374e71a2e195342a
chenqiang <u3637089@connect.hku.hk>
2025-07-28 09:54:04 +0000

    Update download script to fetch individual ImageNet-C dataset components; streamline extraction process.

--- data/download_data.sh
+++ data/download_data.sh
@@ -3,15 +3,27 @@
 # This script downloads the ImageNet-C dataset.
 
 # Create a data directory if it doesn't exist
-# mkdir -p data
+mkdir -p ImageNet-C
+cd ImageNet-C
 
 # Download the ImageNet-C dataset
 # You will need to replace the following URL with the actual download link from the ImageNet website.
 # The file is ~76GB.
-wget -c https://zenodo.org/record/2235448/files/imagenet-c.tar 
+wget -c https://zenodo.org/records/2235448/files/blur.tar
+tar -xvf blur.tar
+rm blur.tar
+
+wget -c https://zenodo.org/records/2235448/files/digital.tar
+tar -xvf digital.tar
+rm digital.tar
+
+wget -c https://zenodo.org/records/2235448/files/noise.tar
+tar -xvf noise.tar
+rm noise.tar
+
+wget -c https://zenodo.org/records/2235448/files/weather.tar
+tar -xvf weather.tar
+rm weather.tar
 
 # Download the Tiny ImageNet-C dataset for smaller experiments
-# wget https://zenodo.org/records/2536630/files/Tiny-ImageNet-C.tar?download=1
-
-# Extract the dataset
-tar -xvf imagenet-c.tar
+# wget https://zenodo.org/records/2536630/files/Tiny-ImageNet-C.tar
